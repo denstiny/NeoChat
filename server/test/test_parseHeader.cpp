ebaebad1ec60200817c9b9069c06cc0da893a5c9
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-04-08 14:32:55 +0800

    chore: use gtest
    Log: use gtest

--- server/test/test_parseHeader.cpp
+++ server/test/test_parseHeader.cpp
@@ -1,8 +1,13 @@
 #include "Message/MessageProcessor.hpp"
+#include <gtest/gtest.h>
 
-int main (int argc, char* argv[]) {
+class Tst_ParseHead: public testing::Test
+{
+
+};
+
+TEST_F(Tst_ParseHead, ParseTest) {
   std::string str = "status: post\r\n";
   Map a =  parseMessageRequestHeaders (str);
   std::cout << a.at ("status") << std::endl;
-  return 0;
 }
